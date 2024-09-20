a13296f8a06ce19645a8e565beb806abf268fd88
Charin Eawzipo <e.charin@ark-insights.com>
2024-09-20 16:21:57 +0700

    fix check_app

--- app/aig/Application/check_app.php
+++ app/aig/Application/check_app.php
@@ -20,9 +20,9 @@ if (($campaign_id && $import_id && $agent_id && $calllist_id) || $id) {
 
   $SQL = "";
   if ($id) {
-    $SQL = "select id,incident_status,campaign_id,calllist_id,import_id,agent_id from t_aig_app where id = '$id'";
+    $SQL = "select * from t_aig_app where id = '$id'";
   } else {
-    $SQL = "select id,incident_status,campaign_id from t_aig_app where campaign_id = '$campaign_id' and  calllist_id = '$calllist_id' and import_id = '$import_id' limit 1 ";
+    $SQL = "select * from t_aig_app where campaign_id = '$campaign_id' and  calllist_id = '$calllist_id' and import_id = '$import_id' limit 1 ";
   }
   wlog($SQL);
   $result = mysqli_query($Conn, $SQL) or die("ไม่สามารถเรียกดูข้อมูลได้");
@@ -57,7 +57,7 @@ if (($campaign_id && $import_id && $agent_id && $calllist_id) || $id) {
       $newapp = "newApplication.php";
   }
 
-  echo "<script>window.location='singaporeApplication.php?campaign_id=$campaign_id&calllist_id=$calllist_id&agent_id=$agent_id&import_id=$import_id&id=$id&formType=ah';</script>";
+  echo "<script>window.location='singaporeApplication.php?campaign_id=$campaign_id&calllist_id=$calllist_id&agent_id=$agent_id&import_id=$import_id&id=$app_id&formType=ah';</script>";
   // if ($chk > 0) {
   //   //echo  $SQL;
   //   if ((in_array($AppStatus,["Follow-doc","Follow Doc","QC_Reject","Reconfirm-App","Reconfirm"]) && $lv==1) || $lv>1) {
