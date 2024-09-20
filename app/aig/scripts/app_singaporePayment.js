6f7dc0cf6bcf6988db5535ce8bf55992f115a488
Charin Eawzipo <e.charin@ark-insights.com>
2024-09-20 11:38:34 +0700

    handle payable , planid on payment retrieve

--- app/aig/scripts/app_singaporePayment.js
+++ app/aig/scripts/app_singaporePayment.js
@@ -3,7 +3,7 @@ function handlePaymentFrequencyChange(radio) {
 
   const paymentFrequency = radio.value;
   const paymentModeSelect = document.querySelector('select[name="Payment_Mode"]');
-  paymentModeSelect.value = ""; 
+  paymentModeSelect.value = "";
   const options = paymentModeSelect.options;
   for (let i = 0; i < options.length; i++) {
     const option = options[i];
@@ -15,18 +15,18 @@ function handlePaymentFrequencyChange(radio) {
     }
   }
 
-    setPlanPoiValue(paymentFrequency);
+  setPlanPoiValue(paymentFrequency);
 }
 
 function setPlanPoiValue(paymentFrequency) {
   const planPoi = document.querySelector('select[name="planPoi1"]');
 
   if (paymentFrequency === '1') { // Annual
-    planPoi.value = "12"; 
+    planPoi.value = "12";
   } else if (paymentFrequency === '2') { // Monthly
-    planPoi.value = "1"; 
+    planPoi.value = "1";
   } else {
-    planPoi.value = ""; 
+    planPoi.value = "";
   }
 
   console.log("planPoi value set to:", planPoi.value);
@@ -63,7 +63,7 @@ function fetchPaymentOption(paymentMode, paymentFrequency) {
       document.body.classList.remove('loading');
     });
 }
-const handlePaymentGateway =async () => {
+const handlePaymentGateway = async () => {
   //retrieve Quote 
   const objectRetrieve = {
     "channelType": "10",
@@ -72,24 +72,41 @@ const handlePaymentGateway =async () => {
   }
   const responseRetrieve = await fetchRetrieveQuote(objectRetrieve)
   console.log("responseRetrieve:", responseRetrieve)
+  console.log("quotationData:", quotationData)
   if (!responseRetrieve) {
     alert("Quote Retrieval List Operation failed");
+    setTimeout(() => {
+      window.reload()
+    }, 2000)
   } else if (responseRetrieve?.statusCode === "P00") {
-    isRecurring = quotationData?.payment_mode===124 ? 1:0;
-  
-  if (quotationData?.premiumPayable) {
-    const url = `payment.php?amount=${quotationData?.premiumPayable}&is_recurring=${isRecurring}&quoteNo=${quotationData?.quoteNo}`;
-    window.open(url, 'childWindow', 'width=600,height=480');
-  } else {
-    console.log("no price", quotationData?.premiumPayable)
+
+    const responseRetrievePlanId = responseRetrieve?.quoteList[0]?.Policy?.insuredList[0]?.planList[0]?.planId
+    const quotePlanId = quotationData?.insuredList[0]?.personInfo?.planInfo?.planId;
+    if (
+      parseFloat(responseRetrieve?.quoteList[0]?.Policy?.premiumPayable) ===
+      parseFloat(quotationData?.premiumPayable) &&
+      responseRetrieve?.quoteList[0]?.Policy?.productId === quotationData?.productId
+      && parseInt(responseRetrievePlanId) === parseInt(quotePlanId)
+    ) {
+      //same plan and amount
+      isRecurring = quotationData?.payment_mode === 124 ? 1 : 0;
+      if (quotationData?.premiumPayable) {
+        const url = `payment.php?amount=${quotationData?.premiumPayable}&is_recurring=${isRecurring}&quoteNo=${quotationData?.quoteNo}`;
+        window.open(url, 'childWindow', 'width=600,height=480');
+      } else {
+        console.log("no price", quotationData?.premiumPayable)
+      }
+    } else {
+      alert("Product is not available. Please do a recalculation of the quote.")
+    }
+
   }
-  }
-  
-  
+
+
 };
 function showAlert(message) {
   alert(message);
-  setTimeout(()=>{
+  setTimeout(() => {
     window.location.reload();
-  },2000)
+  }, 2000)
 }
