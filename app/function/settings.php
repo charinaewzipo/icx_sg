cd8736d72a68903326d36d340f1738a2f5b601b0
Charin Eawzipo <e.charin@ark-insights.com>
2024-10-16 10:00:50 +0700

    fix setting

--- app/function/settings.php
+++ app/function/settings.php
@@ -1,6 +1,10 @@
 <?php
 
 //Database 
+// $GLOBALS["db_server"] = "localhost";
+// $GLOBALS["db_database"] = "icx_sg";
+// $GLOBALS["db_user"] = "root";
+// $GLOBALS["db_password"] = '';
 $GLOBALS["db_server"] = "195.179.239.102";
 $GLOBALS["db_database"] = "u297918512_icx_sg";
 $GLOBALS["db_user"] = "u297918512_root";
@@ -25,28 +29,28 @@ $GLOBALS["m_password"] = "P@ssw0rd";
 $GLOBALS["m_from_name"] = "ARK-SYSTEM";
 
 //AIG_API
-$GLOBALS["aig_api_premium_url"] = "https://qa.apacnprd.api.aig.com/sg-gateway/eway-rest/premium-calculation";
-$GLOBALS["aig_api_create_quote_url"] = "https://uat.apacnprd.api.aig.com/sg-gateway/eway-rest/v3/confirm-quotation";
-$GLOBALS["aig_api_create_policy_url"] = "https://uat.apacnprd.api.aig.com/sg-gateway/eway-rest/issue-policy";
-$GLOBALS["aig_api_retrieve_quote_url"] = "https://uat.apacnprd.api.aig.com/sg-gateway/eway-rest/v3/retrieve-quote";
-$GLOBALS["aig_api_recalculate_quote_url"] = "https://uat.apacnprd.api.aig.com/sg-gateway/eway-rest/reCalculate-Quotation";
-
-
-// $GLOBALS["aig_api_premium_url"] = "https://qa.apacnprd.api.aig.com/sg-gateway/eway-rest/premium-calculation";
-// $GLOBALS["aig_api_create_quote_url"] = "https://qa.apacnprd.api.aig.com/sg-gateway/eway-rest/v3/confirm-quotation";
-// $GLOBALS["aig_api_create_policy_url"] = "https://qa.apacnprd.api.aig.com/sg-gateway/eway-rest/issue-policy";
-// $GLOBALS["aig_api_retrieve_quote_url"] = "https://qa.apacnprd.api.aig.com/sg-gateway/eway-rest/v3/retrieve-quote";
+// $GLOBALS["aig_api_premium_url"] = "https://uat.apacnprd.api.aig.com/sg-gateway/eway-rest/premium-calculation";
+// $GLOBALS["aig_api_create_quote_url"] = "https://uat.apacnprd.api.aig.com/sg-gateway/eway-rest/v3/confirm-quotation";
+// $GLOBALS["aig_api_create_policy_url"] = "https://uat.apacnprd.api.aig.com/sg-gateway/eway-rest/issue-policy";
+// $GLOBALS["aig_api_retrieve_quote_url"] = "https://uat.apacnprd.api.aig.com/sg-gateway/eway-rest/v3/retrieve-quote";
 // $GLOBALS["aig_api_recalculate_quote_url"] = "https://uat.apacnprd.api.aig.com/sg-gateway/eway-rest/reCalculate-Quotation";
 
+
+$GLOBALS["aig_api_premium_url"] = "https://qa.apacnprd.api.aig.com/sg-gateway/eway-rest/premium-calculation";
+$GLOBALS["aig_api_create_quote_url"] = "https://qa.apacnprd.api.aig.com/sg-gateway/eway-rest/v3/confirm-quotation";
+$GLOBALS["aig_api_create_policy_url"] = "https://qa.apacnprd.api.aig.com/sg-gateway/eway-rest/issue-policy";
+$GLOBALS["aig_api_retrieve_quote_url"] = "https://qa.apacnprd.api.aig.com/sg-gateway/eway-rest/v3/retrieve-quote";
+$GLOBALS["aig_api_recalculate_quote_url"] = "https://qa.apacnprd.api.aig.com/sg-gateway/eway-rest/reCalculate-Quotation";
+
 //token
-$GLOBALS['token_url'] = 'https://uatauth1.customerpltfm.aig.com/oauth2/ausuekxpsCZZKg0Cw1d6/v1/token';
-$GLOBALS['client_id'] = '0oah441lav0OCFPY11d7';
-$GLOBALS['client_secret'] = 'yzE2jf3gECLTElZxs2pSNWdTklUzYtdjg2sfzuf9_O2VFgaAN3Ya669A2tsbhRQG';
-$GLOBALS['scope'] = 'SGEwayPartners_IR';
-// $GLOBALS['token_url'] = 'https://devauth1.customerpltfm.aig.com/oauth2/ausmgmbyeSo2EHuJs1d6/v1/token';
-// $GLOBALS['client_id'] = '0oaftcvxzeeCm0rk11d7';
-// $GLOBALS['client_secret'] = '-qe3hBBkSRPNTCmhkGoXVfdD3UH48g2UFH9O9HzdfSDFnL_AVQgWZ1SQu3UkU1K-';
+// $GLOBALS['token_url'] = 'https://uatauth1.customerpltfm.aig.com/oauth2/ausuekxpsCZZKg0Cw1d6/v1/token';
+// $GLOBALS['client_id'] = '0oah441lav0OCFPY11d7';
+// $GLOBALS['client_secret'] = 'yzE2jf3gECLTElZxs2pSNWdTklUzYtdjg2sfzuf9_O2VFgaAN3Ya669A2tsbhRQG';
 // $GLOBALS['scope'] = 'SGEwayPartners_IR';
+$GLOBALS['token_url'] = 'https://devauth1.customerpltfm.aig.com/oauth2/ausmgmbyeSo2EHuJs1d6/v1/token';
+$GLOBALS['client_id'] = '0oaftcvxzeeCm0rk11d7';
+$GLOBALS['client_secret'] = '-qe3hBBkSRPNTCmhkGoXVfdD3UH48g2UFH9O9HzdfSDFnL_AVQgWZ1SQu3UkU1K-';
+$GLOBALS['scope'] = 'SGEwayPartners_IR';
 
 //payment
 $GLOBALS['merchant'] = 'TEST97498471';
