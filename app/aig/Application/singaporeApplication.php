0e8c2c37b5f9432c339d8110e10a4478e5e10676
Charin Eawzipo <e.charin@ark-insights.com>
2024-11-05 16:35:23 +0700

    change regYear to use on db

--- app/aig/Application/singaporeApplication.php
+++ app/aig/Application/singaporeApplication.php
@@ -887,15 +887,19 @@ where name='Nature of Business'";
                   <option value="">
                     <-- Please select an option -->
                   </option>
-                  <option value="2016">2016</option>
-                  <option value="2017">2017</option>
-                  <option value="2018">2018</option>
-                  <option value="2019">2019</option>
-                  <option value="2020">2020</option>
-                  <option value="2021">2021</option>
-                  <option value="2022">2022</option>
-                  <option value="2023">2023</option>
-                  <option value="2024">2024</option>
+                  <?php
+                  $strSQL = "SELECT * FROM t_aig_sg_lov where name = 'vehicleRegYear'";
+                  $objQuery = mysqli_query($Conn, $strSQL);
+                  while ($objResuut = mysqli_fetch_array($objQuery)) {
+                    $data[] = $objResuut;
+                  ?>
+                    <option value="<?php echo $objResuut["id"]; ?>">
+                      <?php echo $objResuut["description"]; ?>
+                    </option>
+                  <?php
+                  }
+                  ?>
+
                 </select>
 
               <th>&nbsp;</th>
